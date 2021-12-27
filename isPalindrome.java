eec036e8d2ffd95b366271e13d394f241e1ff953
Basil Yusuf <basilyusuf1709@gmail.com>
2021-12-27 01:01:59 -0500

    Add files via upload

---
+++ isPalindrome.java
@@ -0,0 +1,40 @@
+public class isPalindrome {
+    // my attempt
+    // public static boolean isPalindrome(int x) {
+    //     int sum = 0;
+    //     int y = 0;
+    //     int i = 1;
+    //     int k = x;
+    //     while(x != 0){
+    //         y = x;
+    //         y = y % 10;
+    //         sum += y*i;
+    //         x = x / 10;
+    //         i *= 10;
+    //     }
+    //     if(sum == k && k >= 0) return true;
+    //     return false;
+    // }
+     // Special cases:
+        // As discussed above, when x < 0, x is not a palindrome.
+        // Also if the last digit of the number is 0, in order to be a palindrome,
+        // the first digit of the number also needs to be 0.
+        // Only 0 satisfy this property.
+        // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+        // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
+        // since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
+
+    public static boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int revNum = 0;
+        while(x > revNum){
+            revNum = revNum * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revNum || x == revNum/10;
+    }
+    // odd numbers middle digit doesn't matter. we are basically checking 12321, if 12 = 21(reversed)
+    public static void main(String[] args){
+        System.out.println(isPalindrome(12321));
+    }
+}
---
+++ twoSum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int sum = 0;
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i <  nums.length; i++){
+            for(int j = 0; j < nums.length; j++){
+                sum=nums[i]+nums[j]; // check the sum 
+                if(sum == target && i != j){ // num[0] + num[0] is not added
+                    a = i;
+                    b = j;
+                }
+            }
+        }
+        
+        int[] hold = {a, b};
+        return hold;
+    }
+}
