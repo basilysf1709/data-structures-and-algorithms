f1fbf4802bb01e085a634a286d92970c5ac1a584
Basil Yusuf <basilyusuf1709@gmail.com>
2021-12-27 01:17:06 -0500

    Add files via upload

---
+++ profitProblem.java
@@ -0,0 +1,9 @@
+class profitProblem {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        for(int i = 0; i < prices.length-1; i++)
+            for(int j = i+1; j < prices.length; j++)
+                max = (prices[j] - prices[i]) > max ? (prices[j] - prices[i]) : max;
+        return max;
+    }
+}
