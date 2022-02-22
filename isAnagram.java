866e22245f049a6da4f1aaf026e0ab7c87fddad8
Basil Yusuf <basilyusuf1709@gmail.com>
2022-02-22 18:39:47 -0500

    Add files via upload

---
+++ isAnagram.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char arr[] = s.toCharArray();
+        char arr1[] = t.toCharArray();
+        int count = 0;
+        
+        Arrays.sort(arr);
+        Arrays.sort(arr1);
+        
+        if(s.length() != t.length()) return false;
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == arr1[i]) count++;
+        }
+        
+        if(count == arr.length) return true;
+        
+        return false;
+    }
+}
