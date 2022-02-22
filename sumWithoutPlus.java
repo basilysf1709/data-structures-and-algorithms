479b608651c2a78cda26ea99ec8c5956e404a0e5
Basil Yusuf <basilyusuf1709@gmail.com>
2022-02-21 23:08:11 -0500

    Add files via upload

---
+++ sumWithoutPlus.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getSum(int a, int b) {
+        int tmp;
+
+        while(b != 0) {
+            tmp = a;
+            a = a ^ b;
+            b = (tmp & b) << 1;
+        }
+        
+        return a;
+    }
+}
