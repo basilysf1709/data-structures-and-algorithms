d3dc2cfbe61de1c67f14c802fc12cfd3e0f8ee9e
basil yusuf <byusuf@uoguelph.ca>
2022-04-27 18:57:25 -0400

    Commit

--- LeetCode/romanToInteger.java
+++ LeetCode/romanToInteger.java
@@ -1,21 +1,21 @@
-package LeetCode;
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int sum = 0;
-        int a = 0;
-        int b = 0;
-        int[] hold;
-        for(int i = 0; i <  nums.length; i++){
-            for(int j = 0; j < nums.length; j++){
-                sum=nums[i]+nums[j]; // check the sum 
-                if(sum == target && i != j){ // num[0] + num[0] is not added
-                    a = i;
-                    b = j;
-                }
-            }
-        }
+// package LeetCode;
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int sum = 0;
+//         int a = 0;
+//         int b = 0;
+//         int[] hold;
+//         for(int i = 0; i <  nums.length; i++){
+//             for(int j = 0; j < nums.length; j++){
+//                 sum=nums[i]+nums[j]; // check the sum 
+//                 if(sum == target && i != j){ // num[0] + num[0] is not added
+//                     a = i;
+//                     b = j;
+//                 }
+//             }
+//         }
         
-        hold = {a, b};
-        return hold;
-    }
-}
+//         // hold = {a, b};
+//         return hold;
+//     }
+// }
