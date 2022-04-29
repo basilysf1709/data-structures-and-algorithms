c6dc23059c1dad2a902ef4b91d6bc0aeb8308437
basil yusuf <byusuf@uoguelph.ca>
2022-04-29 03:02:28 -0400

    Update Recursion

---
+++ Recursion/printLeaf.py
@@ -0,0 +1,10 @@
+# Algorithm: DFS important
+# printLeaves -> function
+# if root == None: return:
+# if root.left == None and root.right = None: 
+#     print(root.data)
+#     return
+# if root.left != None:
+#     printLeaves(root.left)
+# else root.right != None:
+#     printLeaves(root.right)
