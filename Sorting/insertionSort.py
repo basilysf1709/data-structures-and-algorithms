8364ee5cdd41ef3b7cee1833fa0fcc4e2ca9d197
useracct <basilyusuf1709@gmail.com>
2022-06-26 02:43:34 -0400

    feat: adding prac

--- Sorting/insertionSort.py
+++ Sorting/insertionSort.py
@@ -8,3 +8,14 @@ def insertionSort(nums: List[int]) -> List[int]:
             j -= 1
         nums[j + 1] = key
     return nums
+
+
+def pracInsertionSort(nums: List[int]) -> List[int]:
+    for i in range(1, len(nums)):
+        key = nums[i]
+        j = i - 1
+        while j >= 0 and key < nums[j]:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = key
+    return nums
