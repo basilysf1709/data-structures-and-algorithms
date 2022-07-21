35a428643da3efa757084934b1e274bbf834bd7b
Basil Yusuf <basilyusuf1709@gmail.com>
2022-07-21 06:56:51 -0400

    changes

--- Graph/hasPath.py
+++ Graph/hasPath.py
@@ -7,19 +7,34 @@
 # Time complexity: O(nodes ^ 2)
 # Space complexity: O(nodes)
 
-graph1 = {
+graph = {
     'f': ['g', 'i'],
     'g': ['h'],
     'h': [],
     'i': ['g', 'k'],
     'j': ['i'],
-    'k': []
+    'k': ['a', 'b', 'c'],
+    'a': [],
+    'b': [],
+    'c': []
 }
 
+# Depth First
 def hasPath(graph: dict, src: str, dst: str) -> bool:
     if src == dst: return True
     for neighbour in graph[src]:
         if hasPath(graph, neighbour, dst) == True: return True
     return False
 
-print(hasPath(graph1, 'f', 'k '))
+# Breadth First
+def hasPathBF(graph: dict, src: str, dst: str) -> bool:
+    queue = [src]
+    while len(queue) > 0:
+        curr = queue.pop(0)
+        if curr == dst: return True
+        for neighbour in graph[curr]:
+            queue.append(neighbour)
+    return False
+
+print(hasPath(graph, 'f', 'b'))
+print(hasPathBF(graph, 'f', 'b'))
---
+++ Graph/moreGraphProblems.py
@@ -0,0 +1,40 @@
+from typing import List
+
+# Adjacency List
+def buildGraph(edges: List[List[int]]) -> dict:
+    graph = dict()
+    for edge in edges:
+        a, b = edge[0], edge[1]
+        if not a in graph: graph[a] = []
+        if not b in graph: graph[b] = []
+        graph[a].append(b)
+        graph[b].append(a)
+    return graph
+
+# main function
+def undirectedPath(edges: List[List[int]], nodeA: str, nodeB: str) -> bool:
+    graph = buildGraph(edges)
+    visited = set()
+    return hasPath(graph, nodeA, nodeB, visited)
+
+# checking has path
+def hasPath(graph: dict, src: str, dst: str, visited: set) -> bool:
+    if src == dst: return True
+    if src in visited: return False
+
+    visited.add(src)
+
+    for neighbour in graph[src]:
+        if hasPath(graph, neighbour, dst, visited) == True:
+            return True
+    return False
+
+edges = [
+    ['i', 'j'],
+    ['k', 'i'],
+    ['m', 'k'],
+    ['k', 'l'],
+    ['o', 'n']
+]
+
+print(undirectedPath(edges, 'i', 'm'))
